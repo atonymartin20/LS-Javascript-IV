5011f0a3f0e206a3bd0b8c1790c394f63df49832
Alex Martin <atonymartin20@yahoo.com>
2019-07-30 22:27:47 -0500

    Added PMs and Students.  Still need to test

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -38,7 +38,6 @@ class Person {
 class Instructor extends Person {
     constructor(InstructorAttributes) {
         super(InstructorAttributes);
-        // * Instructor has the following unique props:
         //   * `specialty` what the Instructor is good at i.e. 'redux'
         this.specialty = InstructorAttributes.specialty;
         //   * `favLanguage` i.e. 'JavaScript, Python, Elm etc.'
@@ -55,36 +54,56 @@ class Instructor extends Person {
         console.log(`${student.name} receives a perfect score on ${subject}.`)
     }
     // * Now that our students have a grade build out a method on the Instructor (this will be used by _BOTH_ instructors and PM's) that will randomly add or subtract points to a student's grade. _Math.random_ will help.
-    
+
 }
 
 // #### Student
-
-// * Now we need some students!
-// * Student uses the same attributes that have been set up by Person
-// * Student has the following unique props:
-//   * `previousBackground` i.e. what the Student used to do before Lambda School
-//   * `className` i.e. CS132
-//   * `favSubjects`. i.e. an array of the student's favorite subjects ['Html', 'CSS', 'JavaScript']
-// * Student has the following methods:
-//   * `listsSubjects` a method that logs out all of the student's favoriteSubjects one by one.
-//   * `PRAssignment` a method that receives a subject as an argument and logs out that the `student.name has submitted a PR for {subject}`
-//   * `sprintChallenge` similar to PRAssignment but logs out `student.name has begun sprint challenge on {subject}`
-
-// #### Project Manager
-
-// * Now that we have instructors and students, we'd be nowhere without our PM's
-// * ProjectManagers are extensions of Instructors
-// * ProjectManagers have the following unique props:
-//   * `gradClassName`: i.e. CS1
-//   * `favInstructor`: i.e. Sean
-// * ProjectManagers have the following Methods:
-//   * `standUp` a method that takes in a slack channel and logs `{name} announces to {channel}, @channel standy times!​​​​​
-//   * `debugsCode` a method that takes in a student object and a subject and logs out `{name} debugs {student.name}'s code on {subject}`
-
-// #### Stretch Problem
-
-// * Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
+class Student extends Person {
+    constructor(StudentAttributes) {
+        super(StudentAttributes);
+        //   * `previousBackground` i.e. what the Student used to do before Lambda School
+        this.previousBackground = StudentAttributes.previousBackground;
+        //   * `className` i.e. CS132
+        this.className = StudentAttributes.className;
+        //   * `favSubjects`. i.e. an array of the student's favorite subjects ['Html', 'CSS', 'JavaScript']
+        this.favSubjects = StudentAttributes.favSubjects;
+        // * Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
+        this.grade = StudentAttributes.grade;
+    }
+    //   * `listsSubjects` a method that logs out all of the student's favoriteSubjects one by one.
+    listsSubjects() {
+        this.favSubjects.forEach(subject => {console.log(subject)});
+    }
+    //   * `PRAssignment` a method that receives a subject as an argument and logs out that the `student.name has submitted a PR for {subject}`
+    PRAssignment(subject) {
+        console.log(`${this.name} has submitted a PR for ${subject}`)
+    }
+    //   * `sprintChallenge` similar to PRAssignment but logs out `student.name has begun sprint challenge on {subject}`
+    sprintChallenge(subject) {
+        console.log(`${this.name} has begun sprint challenge on ${subject}`);
+    }
 // * Add a graduate method to a student.
 //   * This method, when called, will check the grade of the student and see if they're ready to graduate from Lambda School
 //   * If the student's grade is above a 70% let them graduate! Otherwise go back to grading their assignments to increase their score.
+}
+
+// #### Project Manager
+class ProjectManager extends Instructor {
+    constructor(ProjectManagerAttributes) {
+        super(ProjectManagerAttributes);
+        //   * `gradClassName`: i.e. CS1
+        this.gradClassName = ProjectManagerAttributes.gradClassName;
+        //   * `favInstructor`: i.e. Sean
+        this.favInstructor = ProjectManagerAttributes.favInstructor;
+    }
+    //   * `standUp` a method that takes in a slack channel and logs `{name} announces to {channel}, @channel standy times!​​​​​
+    standUp(slackChannel) {
+        console.log(`${this.name} announces to ${slackChannel}, @channel standy times!​​​​​`);
+    }
+    //   * `debugsCode` a method that takes in a student object and a subject and logs out `{name} debugs {student.name}'s code on {subject}`
+    debugsCode(student, subject) {
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
+    }
+}
+
+
